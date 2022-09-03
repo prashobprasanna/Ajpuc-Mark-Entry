d15dfcd1b708024523bf65efef2a094739dcf5e1
Prashob Prasanna <102350507+prashobprasanna@users.noreply.github.com>
2022-09-03 12:49:13 +0530

    sse

---
+++ index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Basic Page Info -->
+	<meta charset="utf-8">
+	<title>Tyre Resole Factory Management System</title>
+
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="vendors/styles/core.css">
+	<link rel="stylesheet" type="text/css" href="vendors/styles/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="src/plugins/datatables/css/dataTables.bootstrap4.min.css">
+	<link rel="stylesheet" type="text/css" href="src/plugins/datatables/css/responsive.bootstrap4.min.css">
+	<link rel="stylesheet" type="text/css" href="vendors/styles/style.css">
+
+</head>
+<body>
+	<div class="header">
+		<div class="header-left">
+			<div class="menu-icon dw dw-menu"></div>
+		</div>
+		<div class="header-right">
+			<div class="user-info-dropdown">
+				<div class="dropdown">
+					<a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+						<span class="user-icon">
+							<img src="src/images/admin.png" width="50">
+						</span>
+						<span class="user-name">John Doe</span>
+					</a>
+					<div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+						<a class="dropdown-item" href="#"><i class="dw dw-user1"></i> Profile</a>
+						<a class="dropdown-item" href="#"><i class="dw dw-settings2"></i> Setting</a>
+						<hr>
+						<a class="dropdown-item" href="login1.php"><i class="dw dw-logout"></i> Log Out</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="left-side-bar">
+		<div class="brand-logo">
+			<a href="index.php">
+				<img src="src/images/logo.png" width="50px">
+				<h4 style="color: #f3f3f4;font-size: 20px;padding: 15px">Tyre Shop</h4>
+			</a>
+			<div class="close-sidebar" data-toggle="left-sidebar-close">
+				<i class="ion-close-round"></i>
+			</div>
+		</div>
+		<div class="menu-block customscroll">
+			<div class="sidebar-menu">
+				<ul id="accordion-menu">
+					<li>
+						<a href="index.php" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-house"></span><span class="mtext">Dashboard</span>
+						</a>
+					</li>
+					<li>
+						<a href="clients.php" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-user"></span><span class="mtext">Clients</span>
+						</a>
+					</li>
+					<li>
+						<a href="technician.php" class="dropdown-toggle no-arrow">
+							<span class="micon fa fa-wrench"></span><span class="mtext">Technician</span>
+						</a>
+					</li>
+					<li>
+						<a href="services.php" class="dropdown-toggle no-arrow">
+							<span class="micon fa fa-handshake-o"></span><span class="mtext">Services</span>
+						</a>
+					</li>
+					<li>
+						<a href="attendence.php" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-file"></span><span class="mtext">Attendence</span>
+						</a>
+					</li>
+					<li>
+						<a href="items.php" class="dropdown-toggle no-arrow">
+							<span class="micon fa fa-cart-plus"></span><span class="mtext">Items</span>
+						</a>
+					</li>
+					<li>
+						<a href="work-order.php" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-shopping-basket"></span><span class="mtext">Work Order</span>
+						</a>
+					</li>
+					<li>
+						<a href="payment.php" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-money"></span><span class="mtext">Payment</span>
+						</a>
+					</li>
+					<li>
+						<a href="Payment Method.php" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-wallet"></span><span class="mtext">Payment Method</span>
+						</a>
+					</li>
+					<li>
+						<a href="settings.php" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-settings2"></span><span class="mtext">Settings</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="mobile-menu-overlay"></div>
+
+	<div class="main-container">
+		<div class="xs-pd-20-10 pd-ltr-20">
+
+			<div class="title pb-20">
+				<h2 class="h3 mb-0">Dashboard</h2>
+			</div>
+
+			<div class="row pb-10">
+				<div class="col-xl-3 col-lg-3 col-md-6 mb-20">
+					<div class="card-box height-100-p widget-style3">
+						<div class="d-flex flex-wrap">
+							<div class="widget-data">
+								<div class="weight-700 font-24 text-dark">75</div>
+								<div class="font-14 text-secondary weight-500">Work Order</div>
+							</div>
+							<div class="widget-icon">
+								<div class="icon" data-color="#00eccf"><i class="icon-copy fa fa-tasks"></i></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-3 col-lg-3 col-md-6 mb-20">
+					<div class="card-box height-100-p widget-style3">
+						<div class="d-flex flex-wrap">
+							<div class="widget-data">
+								<div class="weight-700 font-24 text-dark">221</div>
+								<div class="font-14 text-secondary weight-500">Client</div>
+							</div>
+							<div class="widget-icon">
+								<div class="icon" data-color="#ff5b5b"><span class="micon fa fa-users"></span></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-3 col-lg-3 col-md-6 mb-20">
+					<div class="card-box height-100-p widget-style3">
+						<div class="d-flex flex-wrap">
+							<div class="widget-data">
+								<div class="weight-700 font-24 text-dark">10</div>
+								<div class="font-14 text-secondary weight-500">Technician</div>
+							</div>
+							<div class="widget-icon">
+								<div class="icon" data-color="#2c515b"><span class="micon fa fa-wrench"></span></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-3 col-lg-3 col-md-6 mb-20">
+					<div class="card-box height-100-p widget-style3">
+						<div class="d-flex flex-wrap">
+							<div class="widget-data">
+								<div class="weight-700 font-24 text-dark">50,000.00</div>
+								<div class="font-14 text-secondary weight-500">Earning</div>
+							</div>
+							<div class="widget-icon">
+								<div class="icon" data-color="#09cc06"><i class="icon-copy fa fa-money" aria-hidden="true"></i></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- js -->
+	<script src="vendors/scripts/core.js"></script>
+	<script src="vendors/scripts/script.min.js"></script>
+	<script src="vendors/scripts/process.js"></script>
+	<script src="vendors/scripts/layout-settings.js"></script>
+	<script src="src/plugins/apexcharts/apexcharts.min.js"></script>
+	<script src="src/plugins/datatables/js/jquery.dataTables.min.js"></script>
+	<script src="src/plugins/datatables/js/dataTables.bootstrap4.min.js"></script>
+	<script src="src/plugins/datatables/js/dataTables.responsive.min.js"></script>
+	<script src="src/plugins/datatables/js/responsive.bootstrap4.min.js"></script>
+	<script src="vendors/scripts/dashboard3.js"></script>
+</body>
+</html>
