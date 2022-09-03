cef8a6963c5de601f5e4053c247f40304b1d552f
Prashob Prasanna <102350507+prashobprasanna@users.noreply.github.com>
2022-09-03 12:50:45 +0530

    e

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+$serverame = "localhost";
+$username = "root";
+$password = "";
+$db_name = "factorydb";
+$con = mysqli_connect($serverame, $username, $password, $db_name)or die("connection failed");
+?>
