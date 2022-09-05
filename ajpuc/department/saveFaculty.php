afa3cc2ad130b7f213acd6f2008e82ae1c1fe7e8
Bhavana-Deshkodi <97719318+Bhavana-Deshkodi@users.noreply.github.com>
2022-09-05 12:23:34 +0530

    faculty page updated

--- ajpuc/department/saveFaculty.php
+++ ajpuc/department/saveFaculty.php
@@ -5,7 +5,8 @@ include("dbconfig.php");
 //$url = "../img/faculty/$fnum.$type";
 //move_uploaded_file($_FILES['userImage']['tmp_name'], $url);
 
-$sql = "INSERT INTO `faculty` (`FName`,'LName','Phone_No','Email-ID','Status','Gender','Qualification','Password') VALUES ('$FName', '$LName', '$Phone_No', '$Email_ID', '$Status', '$Gender', '$Qualification','$Password')";
+$sql = "INSERT INTO `faculty` (`Fname`,`Lname`,`Phone_No`,`Email_ID`,`Status`,`Gender`,`Qualification`,`password`) 
+VALUES ('$fname', '$lname', '$fnum', '$fcemail', '$status', '$gender', '$highqual','$fcpass')";
 $result = $con->query($sql);
 //$last_id = $con->insert_id;
 if($result == True ){
